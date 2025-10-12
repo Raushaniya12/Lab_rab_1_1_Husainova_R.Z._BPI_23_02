664b0b568cf59e7b67fcf20217b475093747a4fa
Raushaniya12 <rausaniahusainova724@gmail.com>
2025-10-12 17:17:23 +0500

    3 labb

--- Lab_rab_1_1_Husainova_R.Z._BPI_23_02/MainWindow.xaml.cs
+++ Lab_rab_1_1_Husainova_R.Z._BPI_23_02/MainWindow.xaml.cs
@@ -128,12 +128,15 @@ namespace Lab_rab_1_1_Husainova_R.Z._BPI_23_02
         private void BtnLight_Click(object sender, RoutedEventArgs e)
         {
             SwitchTheme("LightTheme");
-            
+            BtnLight.Tag = "Active";
+            BtnDark.Tag = null;
         }
+
         private void BtnDark_Click(object sender, RoutedEventArgs e)
         {
             SwitchTheme("DarkTheme");
-            
+            BtnDark.Tag = "Active";
+            BtnLight.Tag = null;
         }
         private void UpdateList()
         {
